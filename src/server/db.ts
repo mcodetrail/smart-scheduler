0e11060a3df3c7485026b986abc40299a9d2ef21
Marco Baldassarri <marco.baldassarri89@gmail.com>
2026-01-25 19:19:03 +0100

    add adapter-better-sqlite3

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,20 @@
 import { env } from "@/env";
 import { PrismaClient } from "../../generated/prisma";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-const createPrismaClient = () =>
-  new PrismaClient({
+const createPrismaClient = () => {
+  const adapter = new PrismaBetterSqlite3({ url: env.DATABASE_URL });
+  return new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-export const db = globalForPrisma.prisma ?? createPrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db: PrismaClient = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
